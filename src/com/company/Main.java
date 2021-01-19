ad646b85743a10eec67bbe34561c91ed3d074e0c
Laura Strouhal <laura_strouhal@hotmail.com>
2021-01-19 16:39:58 +0000

    Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+
+
+    int var1 = 5;
+    int var2 = 6;
+    int sum1 = var1 + var2;
+    System.out.println("The sum of " + var1 + " and " + var2 + " is " + sum1);
+    }
+}
+
