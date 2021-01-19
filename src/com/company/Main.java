a1a9ace05b25a4fd59905207d02d43406241631f
Laura Strouhal <laura_strouhal@hotmail.com>
2021-01-19 18:12:56 +0000

    Adding_Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
     int var2 = 6;
     int sum1 = var1 + var2;
     System.out.println("The sum of " + var1 + " and " + var2 + " is " + sum1);
+
+    System.out.println("Just writing a bit more code.");
     }
 }
 
